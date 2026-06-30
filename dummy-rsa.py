230f9e2c4cd783662e42457fd54d5f20e311380e
Saurav6116 <Saurav.B@upsqrmail.com>
2026-06-30 12:17:06 +0530

    QuantumGuard: Replace RSA import and calls with PQC helpers in dummy-rsa.py

--- dummy-rsa.py
+++ dummy-rsa.py
@@ -1,14 +1,14 @@
-import rsa
+import pqc_helper
 
 # Generate a new 2048-bit RSA keypair
-(pubkey, privkey) = rsa.newkeys(2048)
+(pubkey, privkey) = pqc_helper.generate_pqc_kem_keypair(2048)
 
 message = "Hello, this is a secret classical message."
 
 # Encrypt the message using the public key
-encrypted_msg = rsa.encrypt(message.encode('utf-8'), pubkey)
+encrypted_msg = pqc_helper.pqc_encrypt_payload(message.encode('utf-8'), pubkey)
 print("Encrypted payload:", encrypted_msg)
 
 # Decrypt the message using the private key
-decrypted_msg = rsa.decrypt(encrypted_msg, privkey)
+decrypted_msg = pqc_helper.pqc_decrypt_payload(*encrypted_msg, privkey)
 print("Decrypted payload:", decrypted_msg.decode('utf-8'))
